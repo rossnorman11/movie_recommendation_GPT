870088df2607f0ab3e5cdd79cc3d1614aa413be2
Philipp Zschoernig <p@zschoernig.de>
2024-02-08 19:20:50 +0100

    changes in encoder, Makefile, started API

---
+++ movie_recom/api/fast.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allowing all middleware is optional, but good practice for dev purposes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
+@app.get("/predict")
+def predict(
+        prompt: str, # prompt
+        n_recom: int # number of recommendations
+    ):
+    """
+    gives a list of n_recom recommendations based on the prompt
+    """
+    pass  # YOUR CODE HERE
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the MovieRecommendation API"}
