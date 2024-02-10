a62c20d59b42f6b596b0d8e6ef55ecfd4ec07d29
Philipp Zschoernig <p@zschoernig.de>
2024-02-10 01:05:54 +0100

    first running docker file

--- movie_recom/interface/main.py
+++ movie_recom/interface/main.py
@@ -6,8 +6,8 @@ from colorama import Fore, Style
 
 from movie_recom.params import *
 from movie_recom.ml_logic.encoders import mini_lm_encode, bert_encode
-from movie_recom.ml_logic.data import get_raw_data, get_embedded_data, save_embedded_data
-from movie_recom.ml_logic.model import find_n_nearest_neighbors
+from movie_recom.ml_logic.data import get_data, save_data
+from movie_recom.ml_logic.model import fit_n_nearest_neighbors, predict_n_nearest_neighbors
 from movie_recom.ml_logic.preprocessor import shorten_synopsis
 import requests
 
@@ -18,18 +18,15 @@ def embed_data():
     embed the data
     """
     # get the data from data.get_raw_data
-    df = get_raw_data()
+    df = get_data("raw_data/mpst_full_data.csv")
     # Process data
     # shorten the synopsis with preprocessor.shorten_synopsis
     df = shorten_synopsis(max_len=500, df=df)
+    # embed the synopsis with encoders and saves it
+    df_encoded, df_index = mini_lm_encode(df)
+    save_data(df_encoded, 'processed_data/data_embedded.csv')
+    save_data(df_index, 'processed_data/data_titlenames.csv')
 
-    # embed the synopsis with encoders.mini_lm_encode
-    #df_embedded = mini_lm_encode(df)
-
-    # embed the synopsis with encoders.bert_encode
-    df_embedded = bert_encode(df)
-
-    save_embedded_data(df_embedded)
 
 def embed_prompt(prompt: str) -> pd.DataFrame:
     """
@@ -38,26 +35,28 @@ def embed_prompt(prompt: str) -> pd.DataFrame:
     #put it into a dataframe
     prompt_df = pd.DataFrame({'title': ['prompt'], 'plot_synopsis': [prompt]})
     #embed the prompt with encoders.mini_lm_encode
-    #prompt_embedded = mini_lm_encode(prompt_df)
-
-    #embed the prompt with encoders.bert_encode
-    prompt_embedded = bert_encode(prompt_df)
-
+    prompt_embedded, df_index = mini_lm_encode(prompt_df)
     return prompt_embedded
 
-def recommend(prompt: str="love story in London", n_neighbors: int = 5) -> list:
+def fit_model(n_neighbors: int = 10):
+    '''
+    fit the model
+    '''
+    # get the embedded data
+    df_embedded = get_data("processed_data/data_embedded.csv")
+    # fit the model with model.fit_n_nearest_neighbors
+    fit_n_nearest_neighbors(n=n_neighbors, df_embedded=df_embedded)
+
+def predict(prompt: str = 'love story in sweden without happy ending', n_neighbors: int = 5) -> list:
     '''
     get the prompt and recommend movies based on it
     '''
     # get the embedded prompt
     prompt_embedded = embed_prompt(prompt)
 
-    # get the embedded data
-    df_embedded = get_embedded_data()
-
     # find the nearest neighbors with model.find_n_nearest_neighbors
-    recom_list = find_n_nearest_neighbors(n=n_neighbors, prompt_embedded=prompt_embedded, df_embedded=df_embedded)
-    print(recom_list)
+    recom_list = predict_n_nearest_neighbors(n_neighbors=n_neighbors, prompt_embedded=prompt_embedded)
+    # print(recom_list)
     return recom_list
 
 def call_api():
@@ -70,7 +69,11 @@ def call_api():
 
     response = requests.get(url, params=params)
     response.json() #=> {wait: 64}
-    print(response.json())
+    # print(response.json())
+
+def test():
+    pass
 
 if __name__ == '__main__':
     pass
+    # test()
--- movie_recom/ml_logic/model.py
+++ movie_recom/ml_logic/model.py
@@ -1,20 +1,41 @@
 import numpy as np
 import pandas as pd
-import time
 from colorama import Fore, Style
+import pickle
+from pathlib import Path
+from movie_recom.params import *
 
 from sklearn.neighbors import NearestNeighbors
 
-def find_n_nearest_neighbors(n: int, prompt_embedded: pd.DataFrame, df_embedded: pd.DataFrame):
+def fit_n_nearest_neighbors(n: int, df_embedded: pd.DataFrame):
     '''
     finds n nearest neighbors using the NearestNeighbors algorithm
     '''
     #instatiate model
     nbrs = NearestNeighbors(n_neighbors=n, algorithm='ball_tree').fit(df_embedded)
+    # save in pickle
+    # Define the path and foldername to save the data
+    model_path = Path(PARENT_FOLDER_PATH).joinpath("saved_models/nbrs.pkl")
+    with open(model_path, 'wb') as f:
+        pickle.dump(nbrs, f)
+
+
+def predict_n_nearest_neighbors(n_neighbors: int ,prompt_embedded: pd.DataFrame):
+    '''
+    finds n nearest neighbors using the NearestNeighbors algorithm
+    '''
+    #load pickle model
+    # Define the path and foldername to save the data
+    model_path = Path(PARENT_FOLDER_PATH).joinpath("saved_models/nbrs.pkl")
+    with open(model_path, 'rb') as f:
+        loaded_nbrs = pickle.load(f)
     #find the n nearest neighbors
-    distances, indices = nbrs.kneighbors(prompt_embedded)
+    distances, indices = loaded_nbrs.kneighbors(prompt_embedded, n_neighbors=n_neighbors)
     # generate output list
     list_of_titles = []
+    # load list of titles
+    filepath = Path(PARENT_FOLDER_PATH).joinpath("processed_data/data_titlenames.csv")
+    df_titles = pd.read_csv(filepath, index_col=0)
     for index in indices[0][0:]:
-        list_of_titles.append(df_embedded.iloc[index].name)
+        list_of_titles.append(df_titles.iloc[index][0])
     return list_of_titles
