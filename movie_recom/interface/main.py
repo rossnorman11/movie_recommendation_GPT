78b8c47ed5ed66b723fbe64c868f79ca04ca8873
Ross Norman <ross_norman11@hotmail.com>
2024-02-13 15:52:53 +0000

    Updated merge_prompt_with_favorites

--- movie_recom/interface/main.py
+++ movie_recom/interface/main.py
@@ -19,18 +19,23 @@ def embed_prompt(prompt: str) -> pd.DataFrame:
     prompt_embedded = bert_encode(prompt)
     return prompt_embedded
 
-def merge_promt_with_favorits(prompt_embedded: pd.DataFrame, favs: list) -> pd.DataFrame:
+def merge_prompt_with_favorites(prompt_embedded: pd.DataFrame, favs: list) -> pd.DataFrame:
     # get the embedded data
     # TODO: Adjust to latest models
-    if EMBEDDING_TYPE == 'mini':
-        df_embedded = get_data('processed_data/data_mini_embedded.csv')
-        df_filtered = df_embedded[df_embedded.index.isin(favs)] # embedded dataframe with just the favorites
-        series = prompt_embedded.iloc[0,:] # convert the prompt dataframe to a series
-        df_filtered.loc['prompt'] = series.to_list() # add the prompt to the dataframe (concat didnt work well)
-        mean_df = df_filtered.mean(axis=0).to_frame().T # get the mean of the dataframe, keep it as dataframe
-        mean_df.index = ['prompt'] # set the index to 'prompt'
-        return mean_df
-    return prompt_embedded
+    # Load titles
+    filepath_title = Path(PARENT_FOLDER_PATH).joinpath("raw_data/movie_title.pkl")
+    titles = pd.read_pickle(filepath_title)
+
+    # Load embedded plots
+    filepath_plot = Path(PARENT_FOLDER_PATH).joinpath("processed_data/embeddings_plot.npy")
+    plot_embedded = pd.DataFrame(np.load(filepath_plot), columns=[str(i) for i in range(0,128,1)], index = titles.values)
+
+    df_filtered = plot_embedded[plot_embedded.index.isin(favs)] # embedded dataframe with just the favorites
+    series = prompt_embedded.iloc[0,:] # convert the prompt dataframe to a series
+    df_filtered.loc['prompt'] = series.to_list() # add the prompt to the dataframe (concat didnt work well)
+    mean_df = df_filtered.mean(axis=0).to_frame().T # get the mean of the dataframe, keep it as dataframe
+    mean_df.index = ['prompt'] # set the index to 'prompt'
+    return mean_df
 
 def find_recommendation_vector(text):
     # Vectorise user input
